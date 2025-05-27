0f039f9249ae16201b9f4831a90cd6cf71a28172
Jesus Burga <jeburga9708@gmail.com>
2025-05-26 19:57:22 -0500

    Limpiando alertas del registro

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const skills = document.querySelector('.lista-conocimientos');
+document.addEventListener("DOMContentLoaded", () => {
+  const skills = document.querySelector(".lista-conocimientos");
 
-    if(skills){
-        skills.addEventListener('click', agregarSkills);
+  // Limpiar las alertas
+  let alertas = document.querySelector(".alertas");
 
-        // una vez que estamos en editar, llamar a la funcion
-        skillSeleccionados()
-    }
-})
+  if (alertas) {
+    limpiarAlertas();
+  }
+
+  if (skills) {
+    skills.addEventListener("click", agregarSkills);
+
+    // una vez que estamos en editar, llamar a la funcion
+    skillSeleccionados();
+  }
+});
 
 const skills = new Set();
-const agregarSkills = e => {
-    if(e.target.tagName === 'LI'){
-        skills.add(e.target.textContent);
-        if (e.target.classList.contains('activo')){
-            skills.delete(e.target.textContent);
-            e.target.classList.remove('activo');
-        } else {
-            e.target.classList.add('activo')
-        }
-    } 
+const agregarSkills = (e) => {
+  if (e.target.tagName === "LI") {
+    skills.add(e.target.textContent);
+    if (e.target.classList.contains("activo")) {
+      skills.delete(e.target.textContent);
+      e.target.classList.remove("activo");
+    } else {
+      e.target.classList.add("activo");
+    }
+  }
 
-    document.querySelector('#skills').value = [...skills].join(',');
-}
+  document.querySelector("#skills").value = [...skills].join(",");
+};
 
 const skillSeleccionados = () => {
-    const seleccionadas = Array.from(document.querySelectorAll('.lista-conocimientos .activo'));
+  const seleccionadas = Array.from(
+    document.querySelectorAll(".lista-conocimientos .activo")
+  );
 
-    seleccionadas.forEach(seleccionada => {
-        skills.add(seleccionada.textContent);
-    })
-    
+  seleccionadas.forEach((seleccionada) => {
+    skills.add(seleccionada.textContent);
+  });
 
-    // inyectarlo en hidden
-    const skillsArray = [...skills];
-    document.querySelector('#skills').value = skillsArray;
-}
+  // inyectarlo en hidden
+  const skillsArray = [...skills];
+  document.querySelector("#skills").value = skillsArray;
+};
+
+const limpiarAlertas = () => {
+  const alertas = document.querySelector(".alertas");
+
+  const interval = setInterval(() => {
+    // Verificar si hay alertas para eliminar
+    if (alertas.children.length > 0) {
+      // Eliminar la primera alerta
+      alertas.removeChild(alertas.children[0]);
+    } else if (alertas.children.length === 0) {
+        alertas.parentElement.removeChild(alertas);
+        clearInterval(interval);
+    }
+  }, 2000);
+};
